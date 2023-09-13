161d8eeac78daabaee4f7ee7b8aa7ff5b6dff4b5
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-13 22:11:36 +0500

    Add uzwords

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,15 @@ export const options: Options = {
         "pink-sky",
         "red-season",
     ],
-    type: ["words", "sentences", "numbers", "got", "javascript", "python"],
+    type: [
+        "words",
+        "uzwords",
+        "sentences",
+        "numbers",
+        "got",
+        "javascript",
+        "python",
+    ],
 };
 
 export default function Header() {
